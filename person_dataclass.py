7c39dcd74477755b908bb12d14405c2768ae1998
Ritik Gupta <ritik.gupta@nobroker.in>
2023-10-05 00:51:58 +0530

    Release[feat] -> Build Scraper for waconia website

--- person_dataclass.py
+++ person_dataclass.py
@@ -9,7 +9,4 @@ class Person_Data:
     email:str
     department: List[str] = field(default_factory=list)
 
-if __name__=="__main__":
-    data = {'name': 'Williamson, Kasja', 'school_name': ['Laketown Elementary', 'Student Support Services'], 'department': 'Student Support Services', 'phone_number': '(952) 442-0690', 'email': 'kwilliamson@isd110.org'}
-    p1=Person_Data(data['name'],data['school_name'],data['phone_number'],data['email'],data['department'])
-    print(p1)
+
